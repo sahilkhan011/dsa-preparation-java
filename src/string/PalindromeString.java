9fe159576e1e4fdea7949326bde9cd82b4bfbcae
Sahil Khan <sahilkhan468786@gmail.com>
2024-11-10 22:50:24 +0530

    Palindrome String

---
+++ src/string/PalindromeString.java
@@ -0,0 +1,24 @@
+package string;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(s)); // Output: true
+    }
+
+    public static boolean isPalindrome(String s) {
+        // Convert the string to lowercase and remove all non-alphanumeric characters
+        StringBuilder filteredString = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (Character.isLetterOrDigit(c)) {
+                filteredString.append(Character.toLowerCase(c));
+            }
+        }
+
+        // Check if the filtered string is a palindrome
+        String filtered = filteredString.toString();
+        String reversed = filteredString.reverse().toString();
+
+        return filtered.equals(reversed);
+    }
+}
