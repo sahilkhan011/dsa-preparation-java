db1c8063e28d2909d2b69f80226360dbdf048adb
Sahil Khan <sahilkhan468786@gmail.com>
2024-11-14 00:45:26 +0530

    Most Frequent Leetcode Problem Solution with comments

--- src/NumberAndArray/MostFrequentInArray.java
+++ src/NumberAndArray/MostFrequentInArray.java
@@ -5,16 +5,29 @@ import java.util.Arrays;
 public class MostFrequentInArray {
     public static void main(String[] args) {
         int ans = maxFrequency(new int[]{1,4,8,13},5);
+        int ans1 = maxFrequency(new int[]{1},5);
+        System.out.println(ans1);
     }
+
+    // this problem is solved using sliding window.
+    // in this solution we keep tracking of need.
+    // in each iteration increase need and right pointer (i).
+    // whenever need goes to greater than k.
+    // we reduce need and increase left pointer (j), until need is equal or less than k
 
     public static int maxFrequency(int[] nums, int k) {
         Arrays.sort(nums);
         int ans=1;
-        long total = 0;
+        long need = 0;
         for (int i = 1,j=0; i < nums.length; i++) {
-            total += (long) (nums[i] - nums[i - 1]) * (i - j);
-            while (total > k) {
-                total -= nums[i] - nums[j++];
+            // increase need after increasing right pointer
+            need += (long) (nums[i] - nums[i - 1]) * (i - j);
+            // System.out.println(need);
+
+            // decrease need and increase left pointer until need is not smaller or equal to k.
+            while (need > k) {
+                need -= nums[i] - nums[j++];
+                // System.out.println("after reduce need:"+need);
             }
             ans = Math.max(ans, i - j + 1);
         }
