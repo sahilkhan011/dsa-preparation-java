cbdab3b254babdc5622363beadc164d0947b6942
Sahil Khan <sahilkhan468786@gmail.com>
2024-12-05 22:27:37 +0530

    Majority Element solution

---
+++ src/NumberAndArray/FindMajorityElement.java
@@ -0,0 +1,27 @@
+package NumberAndArray;
+
+public class FindMajorityElement {
+    public static void main(String args[]) {
+        int[] arr = {2, 2, 1, 1, 1, 2, 2};
+        int ans = majorityElement(arr);
+        System.out.println("The majority element is: " + ans);
+
+    }
+
+    private static int majorityElement(int[] arr) {
+        int element = 0;
+        int c = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(c==0){
+                c++;
+                element = arr[i];
+            } else if (arr[i]==element) {
+                c++;
+            }
+            else{
+                c--;
+            }
+        }
+        return element;
+    }
+}
