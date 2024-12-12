bb69869f196b570b6ba450f45f38e4ca964b6e4e
Sahil Khan <sahilkhan468786@gmail.com>
2024-12-12 19:48:33 +0530

    rotate arr solve

--- src/NumberAndArray/RotateMatrix.java
+++ src/NumberAndArray/RotateMatrix.java
@@ -24,4 +24,16 @@ public class RotateMatrix {
         }
         return ans;
     }
+    private static int[][] rotateOptimalSolution(int[][] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int temp = 0;
+                temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        return arr;
+    }
 }
