38842e7d0b72d88b7d5c4e77a7af0852aea8d4f7
Sahil Khan <sahilkhan468786@gmail.com>
2024-12-27 22:59:14 +0530

    Merge Two sorted Array optimal solution

--- src/NumberAndArray/MergeTwoSortedArrays.java
+++ src/NumberAndArray/MergeTwoSortedArrays.java
@@ -4,7 +4,8 @@ import java.util.*;
 
 public class MergeTwoSortedArrays {
 
-    public static void merge(long[] arr1, long[] arr2, int n, int m) {
+    // merge two sorted array without extra space..
+    public static void mergeSolutionOne(long[] arr1, long[] arr2, int n, int m) {
 
         // Declare 2 pointers:
         int left = n - 1;
@@ -33,7 +34,7 @@ public class MergeTwoSortedArrays {
         long[] arr1 = {1, 4, 8, 10};
         long[] arr2 = {2, 3, 9};
         int n = 4, m = 3;
-        merge(arr1, arr2, n, m);
+        mergeSolutionOne(arr1, arr2, n, m);
         System.out.println("The merged arrays are:");
         System.out.print("arr1[] = ");
         for (int i = 0; i < n; i++) {
@@ -45,6 +46,53 @@ public class MergeTwoSortedArrays {
         }
         System.out.println();
     }
+
+    // second optimal solution
+    public static void merge(long[] arr1, long[] arr2, int n, int m) {
+
+        // len of the imaginary single array:
+        int len = n + m;
+
+        // Initial gap:
+        int gap = (len / 2) + (len % 2);
+
+        while (gap > 0) {
+            // Place 2 pointers:
+            int left = 0;
+            int right = left + gap;
+            while (right < len) {
+                // case 1: left in arr1[]
+                //and right in arr2[]:
+                if (left < n && right >= n) {
+                    swapIfGreater(arr1, arr2, left, right - n);
+                }
+                // case 2: both pointers in arr2[]:
+                else if (left >= n) {
+                    swapIfGreater(arr2, arr2, left - n, right - n);
+                }
+                // case 3: both pointers in arr1[]:
+                else {
+                    swapIfGreater(arr1, arr1, left, right);
+                }
+                left++; right++;
+            }
+            // break if iteration gap=1 is completed:
+            if (gap == 1) break;
+
+            // Otherwise, calculate new gap:
+            gap = (gap / 2) + (gap % 2);
+        }
+    }
+
+    public static void swapIfGreater(long[] arr1, long[] arr2, int ind1, int ind2) {
+        if (arr1[ind1] > arr2[ind2]) {
+            long temp = arr1[ind1];
+            arr1[ind1] = arr2[ind2];
+            arr2[ind2] = temp;
+        }
+    }
+
+
 
 }
 
