a44e7bd7b288042bb029ef5ac83ce954eec57e72
Sahil Khan <sahilkhan468786@gmail.com>
2025-01-05 22:10:28 +0530

    Recurssion find combination sum two solution

--- src/Recurssion/FindCombinationSum.java
+++ src/Recurssion/FindCombinationSum.java
@@ -1,40 +1,44 @@
 package Recurssion;
 import java.util.*;
-class Solution {
 
-    private void findCombinations(int ind, int[] arr, int target, List < List < Integer >> ans, List < Integer > ds) {
-        if (ind == arr.length) {
-            if (target == 0) {
-                ans.add(new ArrayList < > (ds));
-            }
-            return;
-        }
-
-        if (arr[ind] <= target) {
-            ds.add(arr[ind]);
-            findCombinations(ind, arr, target - arr[ind], ans, ds);
-            ds.remove(ds.size() - 1);
-        }
-        findCombinations(ind + 1, arr, target, ans, ds);
-    }
-    public List < List < Integer >> combinationSum(int[] candidates, int target) {
-        List < List < Integer >> ans = new ArrayList < > ();
-        findCombinations(0, candidates, target, ans, new ArrayList < > ());
-        return ans;
-    }
-}
 public class FindCombinationSum {
     public static void main(String[] args) {
-        int arr[] = {2,3,6,7};
+        int arr[] = {2, 3, 6, 7};
         int target = 7;
-        Solution sol = new Solution();
-        List < List < Integer >> ls = sol.combinationSum(arr, target);
+        List<List<Integer>> combinations = combinationSum(arr, target);
         System.out.println("Combinations are: ");
-        for (int i = 0; i < ls.size(); i++) {
-            for (int j = 0; j < ls.get(i).size(); j++) {
-                System.out.print(ls.get(i).get(j) + " ");
-            }
-            System.out.println();
+        for (List<Integer> combination : combinations) {
+            System.out.println(combination);
         }
+    }
+
+    // Backtracking function to find combinations
+    private static void findCombinations(int ind, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+        // Base case: if we've considered all elements
+        if (target == 0) {
+            ans.add(new ArrayList<>(ds));  // Add the current combination
+            return;
+        }
+        if (ind == arr.length) {
+            return;  // If no more elements to consider
+        }
+
+        // Include the current element if it's smaller than or equal to the target
+        if (arr[ind] <= target) {
+            ds.add(arr[ind]);
+            // Recur with the same index (since we can reuse the same number)
+            findCombinations(ind, arr, target - arr[ind], ans, ds);
+            ds.remove(ds.size() - 1);  // Backtrack
+        }
+
+        // Exclude the current element and move to the next index
+        findCombinations(ind + 1, arr, target, ans, ds);
+    }
+
+    // Wrapper for the backtracking function
+    public static List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        findCombinations(0, candidates, target, ans, new ArrayList<>());
+        return ans;
     }
 }
