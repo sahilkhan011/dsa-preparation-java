0c6883f30d3395254e3fb2fb11b065196a5148a6
Sahil Khan <sahilkhan468786@gmail.com>
2025-01-07 19:29:20 +0530

    Combination sum 3 leetcode problem solution

--- src/Recurssion/CombinationSum3Solution.java
+++ src/Recurssion/CombinationSum3Solution.java
@@ -3,20 +3,16 @@ import java.util.*;
 public class CombinationSum3Solution {
     public List<List<Integer>> combinationSum3(int k, int n) {
         List<List<Integer>> ans = new ArrayList<>();
-        findCombination(1,k,n,ans,new ArrayList<>());
+        findCombinations(1,k,n,ans,new ArrayList<>());
         return ans;
     }
-    public void findCombination(int ind,int k,int n,List<List<Integer>> ans, List<Integer> ds){
-        if(k < 0 && n < 0){
-            return;
-        }
-        if(k == 0 && n == 0){
-            ans.add(new ArrayList<>(ds));
-        }
+    public void findCombinations(int ind,int k,int n,List<List<Integer>> ans, List<Integer> ds){
+        if(k < 0 && n < 0) return;
+        if(k == 0 && n == 0) ans.add(new ArrayList<>(ds));
 
         for(int i = ind; i <= 9; i++){
             ds.add(i);
-            findConbination(i+1,k-1,n-i,ans,ds);
+            findCombinations(i+1,k-1,n-i,ans,ds);
             ds.removeLast();
         }
     }
