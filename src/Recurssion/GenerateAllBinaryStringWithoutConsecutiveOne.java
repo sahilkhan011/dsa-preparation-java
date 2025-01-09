05fc6e4cfd45975acd0264a7ab8d43fa1fb26e55
Sahil Khan <sahilkhan468786@gmail.com>
2025-01-09 10:50:11 +0530

    Generate All Binary String Without Consecutive 1's Solution

--- src/Recurssion/GenerateAllBinaryStringWithoutConsecutiveOne.java
+++ src/Recurssion/GenerateAllBinaryStringWithoutConsecutiveOne.java
@@ -1,11 +1,7 @@
 package Recurssion;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class GenerateAllBinaryStringWithoutConsecutiveOne {
-
     public static void main(String args[]) {
         int k = 3;
         //Calling function fun with argument k
@@ -14,7 +10,6 @@ public class GenerateAllBinaryStringWithoutConsecutiveOne {
             System.out.println(str+",");
         }
     }
-
     private static List<String> generateAllBinaryString(int k) {
         if (k <= 0) {
             return null;
@@ -34,18 +29,15 @@ public class GenerateAllBinaryStringWithoutConsecutiveOne {
             ans.add(Arrays.toString(ds));
             return;
         }
-
         if(ds[ind-1] == '0'){
             ds[ind] = '0';
             generate(ind+1,k,ds,ans);
             ds[ind] = '1';
             generate(ind+1,k,ds,ans);
         }
-
         if(ds[ind-1] == '1'){
             ds[ind] = '0';
             generate(ind+1,k,ds,ans);
         }
-
     }
 }
