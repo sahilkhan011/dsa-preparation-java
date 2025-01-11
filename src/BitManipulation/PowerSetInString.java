559b5b100838064e2465929d5ef931e5ac82507c
Sahil Khan <sahilkhan468786@gmail.com>
2025-01-11 22:54:23 +0530

    all posible substring solution by bit manipulation

--- src/BitManipulation/PowerSetInString.java
+++ src/BitManipulation/PowerSetInString.java
@@ -3,22 +3,36 @@ package BitManipulation;
 import java.util.*;
 
 public class PowerSetInString {
+
     public static ArrayList<String> AllPossibleStrings(String s) {
         int n = s.length();
-        ArrayList<String>ans=new ArrayList<>();
-        for (int num = 0; num < ( 1 << n ); num++) {
-            StringBuilder tmpStr = new StringBuilder();
+        int nn = 1 << n;  // nn = 2^n, total number of subsets
+        ArrayList<String> ans = new ArrayList<>(nn - 1);  // List to store subsets
+
+        // Iterate over all numbers from 1 to (2^n - 1) to generate non-empty subsets
+        for (int num = 1; num < nn; num++) {  // Start from 1 to skip the empty subset
+            StringBuilder subset = new StringBuilder();
+
+            // Check each bit of num to decide whether to include s[i] in the subset
             for (int i = 0; i < n; i++) {
-                // check bit is set or not
-                if((num & (1 << i)) != 0){
-                    tmpStr.append(s.charAt(i));
+                if ((num & (1 << i)) != 0) {
+                    subset.append(s.charAt(i));  // Append character if the bit is set
                 }
             }
-            if(!tmpStr.isEmpty()){
-                ans.add(tmpStr.toString());
-            }
+
+            ans.add(subset.toString());  // Add the subset to the answer list
         }
-        Collections.sort(ans);
+
         return ans;
+    }
+
+    public static void main(String[] args) {
+        String input = "abc";
+        ArrayList<String> subsets = AllPossibleStrings(input);
+
+        // Print all subsets (power set)
+        for (String subset : subsets) {
+            System.out.println(subset);
+        }
     }
 }
