6e1b1fb5d1ea33230d25391c073b4534a8712b4f
Sahil Khan <sahilkhan468786@gmail.com>
2025-01-11 22:36:21 +0530

    Replace Ascii in String Solution

--- src/string/ReplaceAsciiInString.java
+++ src/string/ReplaceAsciiInString.java
@@ -1,29 +1,33 @@
 package string;
 
 public class ReplaceAsciiInString {
-    public static void modifyString(String s) {
-        StringBuilder result = new StringBuilder();
 
+    // Modify the string based on even and odd index rules
+    public static String modifyString(String s) {
+        StringBuilder ans = new StringBuilder();
+
+        // Loop through each character of the string
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
             // If index is even, increase ASCII value by 1
             if (i % 2 == 0) {
-                result.append((char)(c + 1));
+                ans.append((char)(c + 1)); // Increment ASCII for even index
             }
             // If index is odd, decrease ASCII value by 1
             else {
-                result.append((char)(c - 1));
+                ans.append((char)(c - 1)); // Decrement ASCII for odd index
             }
         }
 
-        // Print the result or do something else with it
-        System.out.println(result.toString());
+        // Return the modified string
+        return ans.toString();
     }
 
     public static void main(String[] args) {
-        String input = "abcdef";
-        modifyString(input);  // Output will be: "badcfe"
+        String str = "abcdef";
+        String ans = modifyString(str);
+        // Print the result inside the main method
+        System.out.println(ans);  // Output will be: "badcfe"
     }
-
 }
