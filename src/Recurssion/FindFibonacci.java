52f5636f10686f2dd3d793e68ce4094e290451fa
Sahil Khan <sahilkhan468786@gmail.com>
2025-01-13 21:00:00 +0530

    find fibonacci using recursion..

--- src/Recurssion/FindFibonacci.java
+++ src/Recurssion/FindFibonacci.java
@@ -7,8 +7,7 @@ public class FindFibonacci {
     }
     // this solution is not recommended for best solution because it creates many unnessecery calls and rudundant calculation...
     private static int findFibonacci(int i) {
-        if (i == 1) return 0;  // Base case: F(0) = 0
-        if (i == 2) return 1;  // Base case: F(1) = 1
+        if (i == 0 || i == 1) return i;  // Base case: F(0) = 0 and Base case: F(1) = 1
         return findFibonacci(i - 1) + findFibonacci(i - 2);  // Recursive case
     }
 }
