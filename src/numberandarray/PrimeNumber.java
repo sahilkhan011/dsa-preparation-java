a0c4c23c0af7ea1c4ade7312a84a5d68aa4d9b47
Sahil Khan <sahilkhan468786@gmail.com>
2025-01-23 23:01:54 +0530

    Prime Number Solution

--- src/numberandarray/PrimeNumber.java
+++ src/numberandarray/PrimeNumber.java
@@ -2,16 +2,24 @@ package numberandarray;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        boolean isPrime = isPrimeNumber(45);
+        boolean isPrime = isPrimeNumber(29);
         System.out.println(isPrime);
     }
 
     private static boolean isPrimeNumber(int num) {
-        if(num%2==0){
-            return false;
+        if (num <= 1) {
+            return false; // 0 and 1 are not prime
         }
-        for (int i = 3; i < Math.sqrt(num); i=i+2) {
-            if(num%i==0){
+        if (num == 2 || num == 3) {
+            return true; // 2 and 3 are prime numbers
+        }
+        if (num % 2 == 0 || num % 3 == 0) {
+            return false; // multiples of 2 or 3 are not prime
+        }
+
+        // Check only odd numbers from 5 to sqrt(num), skipping multiples of 2 and 3
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) {
                 return false;
             }
         }
